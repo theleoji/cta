071439ff472a911fa6109aee09baca57488e47fe
Leo Ji <leo@leoji.codes>
2023-04-24 14:09:31 -0500

    Error handling

--- app/routes/dc.station.$stationCode/route.tsx
+++ app/routes/dc.station.$stationCode/route.tsx
@@ -6,38 +6,62 @@ import { useLoaderData } from "@remix-run/react";
 import NextTrainInfo from "./NextTrainInfo";
 import { StationInfo } from "./StationInfo";
 
-export const loader: LoaderFunction = async ({ params }) => {
-  const stationInfoResponse = await fetch(
-    `https://api.wmata.com/Rail.svc/json/jStationInfo?StationCode=${params.stationCode}`,
-    {
-      headers: {
-        api_key: process.env.WMATA_API_KEY ?? "",
-      },
-    }
-  );
-  const stationInfo = await stationInfoResponse.json();
+type LoaderData = {
+  status: "ok" | "error";
+  message?: string;
+  data?: {
+    station: Station;
+    nextTrain: AIMPredictionTrainInfo[];
+  };
+};
 
-  const nextTrainResponse = await fetch(
-    `https://api.wmata.com/StationPrediction.svc/json/GetPrediction/${params.stationCode}`,
-    {
-      headers: {
-        api_key: process.env.WMATA_API_KEY ?? "",
-      },
-    }
-  );
-  const nextTrain = await nextTrainResponse.json();
-
-  return json(
-    {
-      station: stationInfo,
-      nextTrain: nextTrain,
+function fetchWMATAJson(url: string) {
+  return fetch(url, {
+    headers: {
+      api_key: process.env.WMATA_API_KEY ?? "",
     },
-    {
-      headers: {
-        "cache-control": "max-age=3600",
-      },
-    }
-  );
+  }).then((res) => res.json());
+}
+
+export const loader: LoaderFunction = async ({ params }) => {
+  try {
+    const stationInfoUrl = `https://api.wmata.com/Rail.svc/json/jStationInfo?StationCode=${params.stationCode}`;
+    const nextTrainUrl = `https://api.wmata.com/StationPrediction.svc/json/GetPrediction/${params.stationCode}`;
+
+    const [stationInfo, nextTrain] = await Promise.all(
+      [stationInfoUrl, nextTrainUrl].map(fetchWMATAJson)
+    );
+
+    return json(
+      {
+        status: "ok",
+        data: {
+          station: stationInfo,
+          nextTrain: nextTrain.Trains,
+        },
+      } as LoaderData,
+      {
+        headers: {
+          "cache-control": "max-age=3600",
+        },
+      }
+    );
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : error?.toString();
+
+    return json(
+      {
+        status: "error",
+        message: errorMessage ?? "Unknown error",
+      } as LoaderData,
+      {
+        headers: {
+          "cache-control": "max-age=3600",
+        },
+      }
+    );
+  }
 };
 
 export const meta: V2_MetaFunction = ({ data }) => {
@@ -49,16 +73,28 @@ export const meta: V2_MetaFunction = ({ data }) => {
 };
 
 export default function StationCode() {
-  const { station, nextTrain } = useLoaderData<{
-    station: Station;
-    nextTrain: { Trains: AIMPredictionTrainInfo[] };
-  }>();
+  const { status, data, message } = useLoaderData<LoaderData>();
+
+  if (status !== "ok" || !data) {
+    return (
+      <div className="flex flex-col w-full h-auto min-h-screen py-10 px-6 bg-gray-100 dark:bg-gray-900">
+        <div className="max-w-5xl mx-auto w-full">
+          <h3 className="text-2xl font-semibold text-gray-900 dark:text-gray-100">
+            Error fetching data.
+          </h3>
+          {message ? <pre>{message}</pre> : null}
+        </div>
+      </div>
+    );
+  }
+
+  const { station, nextTrain } = data;
 
   return (
     <>
       <StationInfo station={station} />
       <div className="flex flex-col w-full">
-        {nextTrain.Trains.map((train) => {
+        {nextTrain.map((train) => {
           // Highly unlikely that a train will have the same destination, line, and minutes to arrival -- they'd have to be the same train or be on top of each other or something
           const key = `${train.DestinationName}-${train.Line}-${train.Min}`;
 
